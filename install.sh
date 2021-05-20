0d2c0eae9f17c1b98e89664f4530bcfc401a8e6c
a1eaiactaest <piotrrybiec28@gmail.com>
2021-05-21 00:49:44 +0200

    whats installed

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install vim tmux screen neofetch htop v4l2loopback-dkms python3 python3-pip gcc g++ clang snapd 
+sudo snap install vlc sublime-text code 
+sudo apt install xmonad libghc6-xmonad-dev suckless-tools xscreensaver xmobar
